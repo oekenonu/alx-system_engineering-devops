4ac7c8ba5f0d40658e31b783ee4b53076f3879a2
obinna E <obinnaekenonu@gmail.com>
2024-01-01 21:06:49 +0000

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# 100-puppet_ssh_config.pp
+
+# Disable password authentication in SSH
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+}
+
+# Configure SSH client to use the private key
+file_line { 'Declare identity file':
+  path   => '/home/vagrant/.ssh/config',
+  line   => 'IdentityFile ~/.ssh/school',
+  ensure => present,
+}
