9f5945de3bc789e90b416923a3c0500536123bb2
obinna E <obinnaekenonu@gmail.com>
2024-01-29 20:59:09 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
-import sys
+'''
+For a given employee ID, returns information about his/her TODO list progress.
+'''
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+if __name__ == '__main__':
+    import requests
+    import sys
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    task_done = 0
+    task_title = []
+    u_id = sys.argv[1]
+    req = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                       format(u_id)).json()
+    uname = req.get("username")
+    req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                       format(u_id)).json()
+    for item in req:
+        if item.get('completed') is True:
+            task_title.append(item.get('title'))
+            task_done += 1
+    total_task = len(req)
+
+    print('Employee {} is done with tasks({}/{}):'.
+          format(uname, task_done, total_task))
+    for title in task_title:
+        print('\t {}'.format(title))
