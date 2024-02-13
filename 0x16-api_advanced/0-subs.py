ff5c6c427b5b7fa38c037a8baaf01a87601a3823
obinna E <obinnaekenonu@gmail.com>
2024-02-13 20:38:00 +0000

    pycodestyle passed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Module to get reddit subscribers count"""
-
-
 import requests
+
+
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     for a given subreddit"""
-    
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    header = {"User-Agent":"Anything_1.0"}
+    header = {"User-Agent": "Anything_1.0"}
     data = requests.get(url, headers=header, allow_redirects=False)
-    
+
     if data.status_code >= 300:
         return 0
 
