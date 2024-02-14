9f5521d43a6248c2c5ee174c26bf5450d4c48e63
oekenonu <146424918+oekenonu@users.noreply.github.com>
2024-02-14 21:52:19 +0100

    Create 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Create a manifest to fix termintion of phpp.
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
