4d2b1cfc2386e0eb5adc64d2a7934b17eb64baf8
obinna E <obinnaekenonu@gmail.com>
2024-02-14 03:49:30 +0000

    2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module to get reddit top 10 subscribers"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """Queries the Reddit API and returns the top ten
+    number of subscribers for a given subreddit"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    header = {"User-Agent": "Anything_1.0"}
+    params = {"count": count, "after": after}
+    data = requests.get(url, headers=header, 
+                        params=params, allow_redirects=False)
+
+    if data.status_code >= 400:
+        return None
+
+    hot_list = hot_list + [(child.get("data").get("title"))
+         for child in data.json().get("data").get("children")]
+
+    info = data.json()
+    if not info.get("data").get("after"):
+        return hot_list
+
+    return recurse(subreddit, hot_list, info.get("data").get("count"),
+                   info.get("data").get("after"))
